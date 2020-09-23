cf649f658b681a92a43752d9f88cfc97c61313f6
Ayushman-Singh <37467936+Ayushman-Singh@users.noreply.github.com>
2020-09-23 18:25:25 +0530

    Update Sdata.jsx

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -34,29 +34,29 @@ const sdata =[
          title: "Netflix Original Series",
          links:"https://www.netflix.com/in/title/80186863"
      },
-    // {
-    //     id:6,
-    //     sname: "Extraction",
-    //     imgsrc:"https://wallpapercave.com/wp/wp6273243.jpg",
-    //     title: "Netflix Original Series",
-    //     links:"https://www.netflix.com/in/title/80230399"
-    // },
-    // {
-    //     id:7,
-    //     sname: "Stranger Things",
-    //     imgsrc:"https://wallpapercave.com/wp/wp1839578.jpg",
-    //     title: "Netflix Original Series",
-    //     links:"https://www.netflix.com/in/title/80057281"
-    // },
-    // {
-    //     id:8,
-    //     sname: "The Vampire Diaries",
-    //     imgsrc:"https://wallpapercave.com/wp/B1YynMk.jpg",
-    //     title: "Netflix Original Series",
-    //     links:"https://www.netflix.com/in/title/70143860"
-    // },
-    // {
-    //     id:9,
+     {
+        id:6,
+        sname: "Orange is the New Black",
+        imgsrc:"https://wallpapercave.com/wp/wp1883926.jpg",
+        title: "Netflix Original Series",
+        links:"https://www.netflix.com/in/title/70242311"
+    },
+    {
+        id:7,
+        sname: "Ozark",
+        imgsrc:"https://wallpapercave.com/wp/wp3637823.jpg",
+        title: "Netflix Original Series",
+        links:"https://www.netflix.com/title/80117552"
+    },
+    {
+        id:8,
+        sname: "American Crime Story",
+        imgsrc:"https://wallpapercave.com/wp/wp2348985.jpg",
+        title: "Netflix Original Series",
+        links:"https://www.netflix.com/in/title/80083977"
+    },
+     //{
+     //    id:9,
     //     sname: "DARK",
     //     imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
     //     title: "Netflix Original Series",

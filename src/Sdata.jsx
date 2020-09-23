e9a08a0e5aaf75338b7c473e4eed378e06a965bc
Ayushman-Singh <ayushmansingh2010@gmail.com>
2020-09-23 17:25:46 +0530

    My Movie App

---
+++ src/Sdata.jsx
@@ -0,0 +1,116 @@
+const sdata =[
+    {
+        id:1,
+        sname: "DARK",
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "Netflix Original Series",
+        links:"https://www.netflix.com/in/title/80100172"
+    },
+    {
+        id:2,
+        sname: "Extraction",
+        imgsrc:"https://wallpapercave.com/wp/wp6273243.jpg",
+        title: "Netflix Original Series",
+        links:"https://www.netflix.com/in/title/80230399"
+    },
+    {
+        id:3,
+        sname: "Stranger Things",
+        imgsrc:"https://wallpapercave.com/wp/wp1839578.jpg",
+        title: "Netflix Original Series",
+        links:"https://www.netflix.com/in/title/80057281"
+    },
+    {
+        id:4,
+        sname: "The Vampire Diaries",
+        imgsrc:"https://wallpapercave.com/wp/B1YynMk.jpg",
+        title: "Netflix Original Series",
+        links:"https://www.netflix.com/in/title/70143860"
+    },
+    // {
+    //     id:5,
+    //     sname: "DARK",
+    //     imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80100172"
+    // },
+    // {
+    //     id:6,
+    //     sname: "Extraction",
+    //     imgsrc:"https://wallpapercave.com/wp/wp6273243.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80230399"
+    // },
+    // {
+    //     id:7,
+    //     sname: "Stranger Things",
+    //     imgsrc:"https://wallpapercave.com/wp/wp1839578.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80057281"
+    // },
+    // {
+    //     id:8,
+    //     sname: "The Vampire Diaries",
+    //     imgsrc:"https://wallpapercave.com/wp/B1YynMk.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/70143860"
+    // },
+    // {
+    //     id:9,
+    //     sname: "DARK",
+    //     imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80100172"
+    // },
+    // {
+    //     id:10,
+    //     sname: "Extraction",
+    //     imgsrc:"https://wallpapercave.com/wp/wp6273243.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80230399"
+    // },
+    // {
+    //     id:11,
+    //     sname: "Stranger Things",
+    //     imgsrc:"https://wallpapercave.com/wp/wp1839578.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80057281"
+    // },
+    // {
+    //     id:12,
+    //     sname: "The Vampire Diaries",
+    //     imgsrc:"https://wallpapercave.com/wp/B1YynMk.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/70143860"
+    // },
+    // {
+    //     id:13,
+    //     sname: "DARK",
+    //     imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80100172"
+    // },
+    // {
+    //     id:14,
+    //     sname: "Extraction",
+    //     imgsrc:"https://wallpapercave.com/wp/wp6273243.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80230399"
+    // },
+    // {
+    //     id:15,
+    //     sname: "Stranger Things",
+    //     imgsrc:"https://wallpapercave.com/wp/wp1839578.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/80057281"
+    // },
+    // {
+    //     id:16,
+    //     sname: "The Vampire Diaries",
+    //     imgsrc:"https://wallpapercave.com/wp/B1YynMk.jpg",
+    //     title: "Netflix Original Series",
+    //     links:"https://www.netflix.com/in/title/70143860"
+    // },
+];
+
+export default sdata;
